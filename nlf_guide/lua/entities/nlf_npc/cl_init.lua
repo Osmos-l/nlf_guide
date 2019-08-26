64aedeb9e1d335c26a9cc7625a8d0002abc50646
O S M O S <36885114+OsmosKesko@users.noreply.github.com>
2019-08-26 21:17:45 +0200

    Add files via upload

--- nlf_guide/lua/entities/nlf_npc/cl_init.lua
+++ nlf_guide/lua/entities/nlf_npc/cl_init.lua
@@ -1,51 +1,28 @@
-include("shared.lua");
-ENT.RenderGroup = RENDERGROUP_BOTH;
-
-
-surface.CreateFont( "Font", {
-	font = "Coolvetica",
-	size = 50,
-	weight = 1000,
-	antialias = true,
-} )
-
-surface.CreateFont( "Font2", {
-    font = "Roboto",
-    size = 20,
-    weight = 1000,
-    antialias = true,
-} )
+include("shared.lua")
+ENT.RenderGroup = RENDERGROUP_BOTH
 
 function ENT:DrawTranslucent()
-	self:Draw();
-end;
+	self:Draw()
+end
 
-function ENT:BuildBonePositions(NumBones, NumPhysBones)
-end;
- 
-function ENT:SetRagdollBones(bIn)
-	self.m_bRagdollSetup = bIn;
-end;
-
-function ENT:DoRagdollBone(PhysBoneNum, BoneNum)
-	--self:SetBonePosition( BoneNum, Pos, Angle )
-end;
-
+local iconInfo = Material( OsGuide.iconInfo )
 function ENT:Draw()
 	self:DrawModel()
 
-	local eye = LocalPlayer():EyeAngles()
-	local Pos = self:LocalToWorld( self:OBBCenter() )+Vector( 0, 0, 50 )
-	local Ang = Angle( 0, eye.y - 90, 90 )
-	local clr = HSVToColor(((CurTime()*10)%360), 0.5, 0.5)
+	local LPlayer = LocalPlayer()
+	local CTime = CurTime()
+	local Pos = self:LocalToWorld( self:OBBCenter() ) + Vector( 0, 0, 50 )
+	local Ang = Angle( 0, LPlayer:EyeAngles().y - 90, 90 )
+	local clr = HSVToColor( ( (CTime * 10 ) % 360 ), 0.5, 0.5 )
 	
-	if self:GetPos():Distance( LocalPlayer():GetPos() ) > 1500 then return end
-	cam.Start3D2D(Pos + Vector( 0, 0, math.sin( CurTime() ) * 2 ), Ang, 0.2)
-		draw.SimpleTextOutlined(nlf.bot.config.namebot,"Font",0,-20,nlf.bot.config.colornamebot,TEXT_ALIGN_CENTER,0,1.5,Color(0, 0, 0, 255) )
-		draw.SimpleTextOutlined(nlf.bot.config.namebot2,"Font2",0,25,Color(clr.r, clr.g, clr.b, 220),TEXT_ALIGN_CENTER,0,1,Color(0, 0, 0, 255) )
+	if ( self:GetPos():Distance( LPlayer:GetPos() ) > 1500 ) then return end
+
+	cam.Start3D2D(Pos + Vector( 0, 0, math.sin( CTime ) * 2 ), Ang, 0.2)
+		draw.SimpleTextOutlined( OsGuide.title, "guide_C_50", 0, -20, OsGuide.titleColor, TEXT_ALIGN_CENTER, 0, 1.5, Color(0, 0, 0, 255) )
+		draw.SimpleTextOutlined( OsGuide.desc, "guide_R_50", 0, 25, Color(clr.r, clr.g, clr.b, 220), TEXT_ALIGN_CENTER, 0, 1, Color(0, 0, 0, 255) )
 		
 		surface.SetDrawColor(255, 255, 255)
-        surface.SetMaterial(Material(nlf.bot.config.icon.info))
+        surface.SetMaterial( iconInfo )
 		surface.DrawTexturedRect(-10, -60, 32, 32)
 	cam.End3D2D()
 end
--- nlf_guide/lua/entities/nlf_npc/shared.lua
+++ nlf_guide/lua/entities/nlf_npc/shared.lua
@@ -3,20 +3,14 @@ ENT.Type = "ai"
 
 ENT.PrintName 	= "Npc Information"
 ENT.Author 		= "Osmos"
-ENT.Contact 	= ""
 ENT.Category	= "NLF"
 
-ENT.AutomaticFrameAdvance = true;
+ENT.AutomaticFrameAdvance = true
    
-ENT.Spawnable = true;
-ENT.AdminSpawnable = true;
+ENT.Spawnable = true
+ENT.AdminSpawnable = true
 
-function ENT:PhysicsCollide(data, physobj)
-end;
-
-function ENT:PhysicsUpdate(physobj)
-end;
 
 function ENT:SetAutomaticFrameAdvance(bUsingAnim)
-	self.AutomaticFrameAdvance = bUsingAnim;
+	self.AutomaticFrameAdvance = bUsingAnim
 end
